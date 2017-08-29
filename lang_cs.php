de0c5e302bad77644126dd3eb978e8282ee0d39d
dannylehcim <31448516+dannylehcim@users.noreply.github.com>
2017-08-29 16:43:11 +0000

    Create lang_cs.php

---
+++ lang_cs.php
@@ -0,0 +1,60 @@
+<?php
+// language: Czech
+// made by: dannylehcim @ snusbase
+// updated on: 22.June.2017
+
+// MENU
+define('_SEARCH', 'hledat');
+define('_PURCHASE', 'koupit');
+define('_SUPPORT', 'podpora');
+define('_ACCOUNT', 'účet');
+define('_ACCOUNT_SETTINGS', 'nastavení účtu');
+define('_ACCOUNT_CHANGE', 'nastavení vzhledu'); //client: website, eg font type, theme
+define('_AFFILIATE', 'affiliate');
+define('_AFFILIATE_NEW', 'zobrazení příjmů');
+define('_AFFILIATE_KEYS', 'změnit klíče');
+
+// ACCOUNT
+define('_SIGN_OUT', 'odhlásit se');
+define('_WELCOME', 'vítejte,'); //eg Welcome, Username.
+
+// SUPPORT
+define('_SUPPORT', 'alternativní metody kontaktu jsou náš Twitter a náš email publicdbhost@gmail.com');
+define('_MOST_ACTIVE', 'naše nejaktivnější doba podpory je');
+define('_MONDAY_FRIDAY', 'Pondělí až Pátek.');
+
+// MEMBERS PAGE
+define('_P_OVERVIEW', 'přehled výkonu');
+define('_AVG_QUERY', 'průměrný dotaz');
+define('_AVG_RESULTS', 'průměrné výsledky');
+define('_ACC_NUMBER', 'číslo účtu');
+define('_API_LOAD', 'zatížení api');
+define('_SHORT_TERM', 'krátkodobé cíly');
+define('_LONG_TERM', 'dlouhodobé cíly');
+define('_MOST_ACTIVE', 'Neustále se snažíme zlepšit výkonnost, proto zveřejňujeme naše statistiky. Statistiky se vynulují na začátku každého měsíce a když provádíme hlavní aktualizace rozhraní API.');
+
+// ACCOUNT PAGE
+define('_CHANGE_USERNAME', 'změnit přihlašovací jméno');
+define('_CHANGE_USERNAME_DISPLAY', 'změnit uživatelské jméno');
+define('_CHANGE_EMAIL', 'změnit email');
+define('_CHANGE_PASSWORD', 'změnit heslo');
+
+// PURCHASE PAGE
+define('_PURCHASE', 'koupit');
+define('_X_DAY_ACCESS', 'denní přístup'); //EG "7 DAY ACCESS"
+define('_SEARCH_FEATURES', 'neomezený přístup k funkcím vyhledávání');
+define('_BASIC_WILDCARD', 'základní divoká karta');
+define('_CUSTOM_WILDCARD', 'vlastní divoká karta');
+define('_PRIORITIZED_SUPPORT', 'prioritná podpora');
+define('_PRIVATE_ENDPOINT', 'soukromý API koncový bod');
+define('_ALL_FEATURES', 'všechny funkce Demo a Premium');
+define('_DAY_EMAIL_SUPPORT', '24/7 emailová podpora');
+
+// AFFILIATE PAGE
+define('_AFFILIATE_WELCOME', 'affiliate systém');
+define('_AFFILIATE_MADE', 'peníze vydělané');
+define('_AFFILIATE_KEY', 'affiliate klíč');
+define('_AFFILIATE_CREATE', 'vytvořit nový klíč');
+define('_AFFILIATE_WITHDRAW', 'vybrat');
+
+?>
